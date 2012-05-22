2283cd0d8c85545b0491f7be4a6b387f19f53372
ken <proxy.vk@gmail.com>
2012-05-22 16:25:49 +0800

    add config.go

---
+++ config.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type ConfigFile struct {
+	filename  string
+	separator string
+	file      *os.File
+	m         map[string]string
+}
+
+func Config(filename string, separator string) (cf *ConfigFile, err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConfigFile{filename, separator, f, make(map[string]string)}, nil
+}
+
+func (cf *ConfigFile) Parse() (map[string]string, error) {
+
+	reader := bufio.NewReader(cf.file)
+	re, err := regexp.Compile("^\\s*([a-zA-Z_][0-9a-zA-Z_]*)\\s*" + cf.separator + "\\s*([^\\s]+)\\s*$")
+	if err != nil {
+		return cf.m, err
+	}
+
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err == nil && isPrefix == false {
+			subs := re.FindSubmatch(line)
+
+			if len(subs) == 3 {
+				str := string(subs[1])
+				cf.m[str] = string(subs[2])
+			}
+
+		} else {
+			break
+		}
+	}
+	return cf.m, err
+}
+
+func (cf *ConfigFile) Get(key string) string {
+	return cf.m[key]
+}
+
+func main() {
+
+	config, err := Config("config.txt", ":")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+
+	c, err := config.Parse()
+	if err == nil {
+		fmt.Printf("==: %s\n", c["logfile"])
+		fmt.Printf("==: %s\n", c["logfiles"])
+	}
+}
