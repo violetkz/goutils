3e7b886d3f8323eaa4e7e4380ebb27b997ca15f7
ken <proxy.vk@gmail.com>
2012-06-28 16:02:51 +0800

    add main func

--- src/e/req.go
+++ src/e/req.go
@@ -1,8 +1,24 @@
-import "net/http"
+package  main;
+
+import  "net/http"
 
 
-func send_req(url string, user string, pass string) ok bool {
-
-r := new http.Request ("HEAD", url)
-    
+func send_req() {
+	client := &http.Client{}
+	//resp, err := client.Get("http://localhost:8080/oneTest/task")
+	req, err := http.NewRequest("GET", "http://localhost:8080/oneTest/task", nil)
+	req.SetBasicAuth("zhoukaiz@cn.ibm.com", "123456")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("send_req failed\n")
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%s", body)
 }
+
+type loginFWInofo struct {
+	user    string
+	psw     string
+	logfile string
+}
+
--- src/test/main.go
+++ src/test/main.go
@@ -1,24 +1,59 @@
 package main
 
 import (
-        "fmt"
-        "config"
-       )
+	"config"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type request struct {
+	method string
+	url    string
+	user   string
+	passwd string
+}
+
+func new_req(method string, url string, user string, pwd string) (req *request) {
+	return &request{method, url, user, pwd}
+}
+
+//FIXME: reutrn value should be Boolean represented success or fail.
+func (req *request) send() ([]byte, error) {
+	client := &http.Client{}
+	http_req, err := http.NewRequest(req.method, req.url, nil)
+	http_req.SetBasicAuth(req.user, req.passwd)
+	resp, err := client.Do(http_req)
+	if err != nil {
+		fmt.Printf("send_req failed\n")
+		return nil, err
+	}
+	return ioutil.ReadAll(resp.Body)
+}
 
 func main() {
 
 	con, err := config.Config("config.txt", ":")
 	if err != nil {
 		fmt.Printf("%s\n", err)
-        return
+		return
 	}
 
 	c, err := con.Parse()
 	if err == nil {
-		fmt.Printf("==: %s\n", c["logfile"])
-        if "" == c["logfiles"] {
-            fmt.Printf("nil\n");
-        }
-		fmt.Printf("==: %s\n", c["logfiles"])
+		fmt.Printf("\nConnect: %s, User: %s\n", c["host"], c["user"])
 	}
+
+	//send_req()
+	r := new_req("HEAD", c["host"], c["user"], c["passwd"])
+	resp, err := r.send()
+	if err == nil {
+		//fmt.Printf("%s", resp)
+		fmt.Printf("\n    OK!     ")
+	} else {
+		fmt.Printf("\n    NG!     ")
+	}
+
+	time.Sleep(4000 * time.Millisecond)
 }
